57d133031da0b66c2fa4bf3e454606bfc203badc
AlejandraVe <152411557+AlejandraVe@users.noreply.github.com>
2025-02-24 16:42:03 +0100

    Update get_next_line.c

--- get_next_line.c
+++ get_next_line.c
@@ -22,7 +22,7 @@ char	*check_new_line(char *str)
 		i++;
 	if (str[i] == '\n')
 		i++;
-	new_line = ft_calloc(i + 1, sizeof(char *));
+	new_line = (char *)malloc(sizeof(char) * (i + 1));
 	if (!new_line)
 		return (NULL);
 	i = 0;
@@ -32,12 +32,12 @@ char	*check_new_line(char *str)
 		i++;
 	}
 	if (str[i] == '\n')
-		new_line[i++] = '\n';
-	new_line[i] = '\0';
+		new_line[i] = '\n';
+	new_line[i + 1] = '\0';
 	return (new_line);
 }
 
-char	*free_line(char *str)
+/*char	*free_line(char *str)
 {
 	char	*new;
 	char	*position;
@@ -63,7 +63,7 @@ char	*free_line(char *str)
 	new[i] = '\0';
 	free(str);
 	return (new);
-}
+}*/
 
 char	*get_next_line(int fd)
 {
@@ -74,42 +74,53 @@ char	*get_next_line(int fd)
 
 	if (fd < 0 || BUFFER_SIZE <= 0)
 		return (NULL);
-	buffer = ft_calloc(BUFFER_SIZE + 1, sizeof(char *));
-	if (!buffer)
-		return (NULL);
 	line_read = NULL;
 	size_read = 1;
-	while (!ft_strchr(line_read, '\n') && 0 < size_read)
+	buffer = (char *)malloc(sizeof(char) * (BUFFER_SIZE + 1));
+	if (!buffer)
+		return (NULL);
+	while (!ft_strchr(line_read, '\n') && size_read != 0)
 	{
 		size_read = read(fd, buffer, BUFFER_SIZE);
 		if (size_read < 0)
 		{
 			free(line_read);
-			line_read = NULL;
 			return (NULL);
 		}
 		buffer[size_read] = '\0';
-		line_read = update_line_read(line_read, buffer);
-		if (!line_read)
-			return (NULL);
+		line_read = ft_strjoin(line_read, buffer);
 	}
-	line = check_new_line(line_read);
-	line_read = free_line(line_read);
-	return (line);
+	free(line_read);
+	buffer = check_new_line(line_read);
+	line_read = update_line(line_read);
+	return (buffer);
 }
 
-char	*update_line_read(char *str, char *buffer)
+char	*update_line(char *str)
 {
 	char	*updated;
+	int	i;
+	int	j;
 
+	i = 0;
+	j = 0;
+	while (str[i] && (str[i] != '\n'))
+		i++;
 	if (!str)
 	{
-		str = malloc(1);
-		str[0] = '\0';
-	}
-	if (!str)
+		free(str);
 		return (NULL);
-	updated = ft_strjoin(str, buffer);
+	}
+	i += (stored[i] == '\n');
+	updated = (char *) malloc(sizeof(char) * (ft_strlen(str) - i + 1));
+	if (!updated)
+		return (NULL);
+	while (str[i + j])
+	{
+		updated[j] = str[i + j];
+		j++;
+	}
+	updated[j] = '\0';
 	free(str);
 	return (updated);
 }
